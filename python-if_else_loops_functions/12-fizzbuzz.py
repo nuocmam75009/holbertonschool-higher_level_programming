a3e650d132ba5ba8402ce1db5d4fef23126258f1
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-07 12:56:11 +0200

    corrected 12 and some other tasks

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,14 +2,14 @@
 def fizzbuzz():
     for i in range(1, 100):
         if i % 3 == 0:
-            print("Fizz")
+            print("Fizz", end=' ')
             continue
         if i % 5 == 0:
-            print("Buzz")
+            print("Buzz", end=' ')
             continue
         if i % 15 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end=' ')
             continue
         else:
-            print(f"{i}")
+            print(f"{i}", end=' ')
             continue
