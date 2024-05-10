61e41bbbe44c4cda11cd1e64e8d2ee9d669eda9d
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-10 20:28:00 +0200

    fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(''.join(chr(i) for i in range(97, 123) if chr(i) not in ['q', 'e']), end='')
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print("{}".format(chr(i)), end='')
