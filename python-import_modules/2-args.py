fb3b60acd2703fcbc21341aac8929e8238095647
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-10 21:32:54 +0200

    fix 2nd

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 import sys
 
+def print_args(args):
+    num_args = len(args)
+    if num_args == 0:
+        print("Number of arguments: 0.")
+    elif num_args == 1:
+        print(f"Number of argument: 1:")
+        print(f"1: {args[0]}")
+    else:
+        print(f"{num_args} arguments:")
+        for i, arg in enumerate(args, start=1):
+            print(f"{i}: {arg}")
+
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1
-    print("{} {}{}".format(argc, "argument" if argc == 1 else "arguments", "." if argc < 1 else ":"))
-    if argc >= 1:
-        for i in range(1, len(sys.argv)):
-            arg = sys.argv[i]
-            print("{}: {}".format(i, arg))
+    print_args(sys.argv[1:])
