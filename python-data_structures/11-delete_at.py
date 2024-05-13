2fd9c555155a8b721ea7aa3fd000710e31975e35
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-13 13:13:31 +0200

    FIX ERROR NOW ITS GOOD MOTHERFUKr

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,5 +3,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        my_list = my_list.remove(idx)
+        element = my_list[idx]
+        my_list.remove(element)
         return my_list
