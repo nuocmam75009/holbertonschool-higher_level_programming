40d5d8f5ed21bc102fe18a6451c94ab1640b8214
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-14 09:17:52 +0200

    fix commenting error

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     return set_1 ^ set_2
-# utilise la built-in func set mais la symmetric_difference qui s'utilise avec ^
+# utilise la built-in func set mais la
+# symmetric_difference qui s'utilise avec ^
