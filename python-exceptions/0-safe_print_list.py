6daecd7df5a0a76b4cec857c69fd5f7c4700c190
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-15 18:10:37 +0200

    fixed the print() that was in the loop

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             number += 1
-            print()
+        print()
     except IndexError:
         print()
     return number
