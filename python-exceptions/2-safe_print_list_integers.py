6858e6e88c9cdaa7cafae20286d4f2db3771bdde
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-15 18:32:40 +0200

    fix pycodestyle error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     number = 0
     try:
         for i in range(x):
-            if isinstance(my_list[i], int): #check if it's an int
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 number += 1
         print()
