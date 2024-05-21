497d668a9b8baa802009afede418b1500134584c
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-21 11:29:59 +0200

    3rd task

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,19 +2,13 @@
 """Prints two strings"""
 
 def say_my_name(first_name, last_name=""):
+    """function say my name
+    values must be strs
+    returns str
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     else:
-        print("My name is {} {}".format(first_name, last_name))
-#!/usr/bin/python3
-"""Prints two strings"""
-
-def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+        print("My name is {:s} {:s}".format(first_name, last_name))
