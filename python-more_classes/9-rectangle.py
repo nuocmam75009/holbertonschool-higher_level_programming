3661633c57686de5f623b1e7d088e3cc468ecb50
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-26 16:14:18 +0200

    oops now i deleted the spaces around the = line 114

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -111,6 +111,6 @@ class Rectangle:
             return rect_2
 
     @classmethod
-    def square(cls, size = 0):
+    def square(cls, size=0):
         """creates a new rectangle"""
         return cls(size, size)
