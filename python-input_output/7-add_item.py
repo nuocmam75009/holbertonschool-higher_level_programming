e32505793e81df5f6c216ed43a9ccb426c7468d3
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 12:53:54 +0200

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,14 +4,12 @@
 
 import sys
 import os.path
-import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
-
 arguments = sys.argv[1:]
 
 try:
@@ -20,5 +18,4 @@ except FileNotFoundError:
     items = []
 
 items.extend(arguments)
-
 save_to_json_file(items, filename)
