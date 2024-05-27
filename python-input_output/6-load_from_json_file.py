e5e9576d66d4a53c365b2fc685e78361c75ce1b3
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 10:50:50 +0200

    added  return

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,4 +10,4 @@ def load_from_json_file(filename):
     converts json file to object
     """
     with open(filename, 'r', encoding='utf(8)') as f:
-        json.load(f)
+       return json.load(f)
