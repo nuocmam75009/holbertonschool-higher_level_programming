536e148c2ae8e9828eb4b0c984872fcf2922c560
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 10:52:29 +0200

    fixed utf

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
     converts json file to object
     """
-    with open(filename, 'r', encoding='utf(8)') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
