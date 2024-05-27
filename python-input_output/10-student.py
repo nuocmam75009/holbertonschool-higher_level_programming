8f5e6641786ad61a3df27e4b507f9b4ac68d72b8
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 12:36:57 +0200

    __dict to __dict__

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,7 +16,7 @@ class Student:
         attrs: list of strings, only attribute names
         """
         if attrs is None:
-            return self.__dict
+            return self.__dict__
         else:
             return {key: value for key, value in self.__dict__.items()
                     if key in attrs}
