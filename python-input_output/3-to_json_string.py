c52477abc5d0a6e4565528b80d7ce12bbd43749b
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 10:18:31 +0200

    JSON representation of my_obj

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""JSON representation"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    function that returns JSON representation
+    of my_obj
+    """
+    json.dumps(my_obj)
