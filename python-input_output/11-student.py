adf885e7cc17a35e82231d31cca4f2e63863396f
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 12:48:39 +0200

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""pourquoi il faut faire ça"""
+
+
+class Student:
+    """defines a defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dict repr of Student
+        attrs: list of strings, only attribute names
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+
+    def reload_from_json(self, json):
+        """replaces all attributes from Student"""
+        for key, value in json.items():
+            setattr(self, key, value)
