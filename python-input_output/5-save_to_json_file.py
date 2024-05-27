c02247bb124c72611e32489c0608165f2a7f490d
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 10:37:50 +0200

    save to json

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""write object to txt"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    function that writes an object to a txt
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return json.dump(my_obj, f)
