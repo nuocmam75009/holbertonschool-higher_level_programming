ac603e54579cfaf7e82eb20481dc5f276769db42
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 09:58:18 +0200

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,7 @@ def write_file(filename='', text=''):
     function that writes a str to a text file
     filename=file being written
     text=text being added
+    Returns number of chars in filename
     """
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
