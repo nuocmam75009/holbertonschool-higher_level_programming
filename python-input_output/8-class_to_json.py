07ef1e712a1353e9eeb085ab301019ad09c67dbc
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 11:22:29 +0200

    8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class to JSON"""
+
+
+def class_to_json(obj):
+    """
+    function that returns the dict for JSON serialisation
+    obj: instance of class
+    """
+    return {key: value for key, value in obj.__dict__.items() if isinstance(value, (str | int | bool | list | dict))}
