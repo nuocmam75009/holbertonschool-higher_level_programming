275f77432b25cbc4d8c020f088c820aceb164727
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 09:56:02 +0200

    writes a file and returns number of chars

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+"""Write a file"""
+
+
+def write_file(filename='', text=''):
+    """
+    function that writes a str to a text file
+    filename=file being written
+    text=text being added
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
