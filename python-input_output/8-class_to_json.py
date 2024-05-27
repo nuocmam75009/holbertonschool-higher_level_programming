b07ad51debc9fa09f42d6e66e43da32b905d7d5e
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 12:09:17 +0200

    return obj dict

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,6 +7,4 @@ def class_to_json(obj):
     function that returns the dict for JSON serialisation
     obj: instance of class
     """
-
-    if isinstance(obj, (str, int, bool, list, dict)):
-        return {key: value for key, value in obj.__dict__.items()}
+    return obj.__dict__
