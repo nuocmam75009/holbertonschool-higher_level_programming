e71d528ee7c7fecdfc7fe4e02d6f836b83d9f732
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 11:11:28 +0200

    fixed

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""load, add, save"""
+"""7. Load, add, save"""
 
 
-import json
 import sys
 import os.path
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 
