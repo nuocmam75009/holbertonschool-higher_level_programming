bf56663842e79c3f2f76cbd68328ed5d066815af
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 10:13:41 +0200

    appending

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Append a string"""
+
+
+def append_write(filename='', text=''):
+    """
+    Appends a string
+    filename=file being appended
+    text=string
+    Returns: number of chars added
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.append(text)
