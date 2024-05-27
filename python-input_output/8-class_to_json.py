49f36deb9f706fb095378274dbbb6180a5bcd929
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-27 11:26:26 +0200

    splitted line into two to fit the documentation requirements

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,4 +7,6 @@ def class_to_json(obj):
     function that returns the dict for JSON serialisation
     obj: instance of class
     """
-    return {key: value for key, value in obj.__dict__.items() if isinstance(value, (str | int | bool | list | dict))}
+    # return {key: value for key, value in obj.__dict__.items() if isinstance(value, (str | int | bool | list | dict))}
+    if isinstance(obj, str | int | bool | list | dict):
+        return {key: value for key, value in obj.__dict__.items()}
