0bc162cab25e3fb0ab3fef2d25136c83bf2ee81d
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-28 12:08:15 +0200

    used pickle

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
-import json
-
+import pickle
 
 def serialize_and_save_to_file(data, filename):
-    """serialize dict to json"""
-    json.dump(data, filename)
+    """function that serializes and saves data
+    data: python dict
+    filename: output JSON file
+    """
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f)
 
 def load_and_deserialize(filename):
-    """deserialize"""
-    json.load(filename)
+    """function that load and deserializes data"""
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
