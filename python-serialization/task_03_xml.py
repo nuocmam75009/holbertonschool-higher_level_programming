e8bf8d7155f00a77eb711bc170a240d09f353903
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-31 10:14:25 +0200

    fix

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,33 +1,27 @@
+#!/usr/bin/python3
+"""3. Serializing and Deserializing with XML"""
+
+
 import xml.etree.ElementTree as ET
 
+
 def serialize_to_xml(dictionary, filename):
-    """create the root element"""
-    root = ET.Element('data')
-    """for each key-value pair in dict, create a subelement"""
+    """serialize python dictionary into XML"""
+    root = ET.Element("data")
     for key, value in dictionary.items():
-        child = ET.SubElement(root.key)
-        child.text = str(value)
-    """create ElementTreee and write to file"""
+        child = ET.SubElement(root, key)
+        child.text = value
     tree = ET.ElementTree(root)
     tree.write(filename)
 
+
 def deserialize_from_xml(filename):
-    """parse XML and get root element"""
+    """read the XML data from that file,
+     and return a deserialized Python dictionary"""
     tree = ET.parse(filename)
     root = tree.getroot()
-    """create dict from XML"""
-    dictionary = {child.tag: child.text for child in root}
-    return dictionary
 
-if __name__ == "__main__":
-    """Defines a dictionary
-    then defines a filename
-    serializes the dict to XML
-    deserializes XML back to dict
-    print the deserialized data
-    """
-    data = {'name': 'John', 'age': 25, 'is_student': True}
-    filename = 'data.xml'
-    serialize_to_xml(data, filename)
-    deserialized_data = deserialize_from_xml(filename)
-    print(deserialized_data)
+    _dict = {}
+    for child in root:
+        _dict[child.tag] = child.text
+    return _dict
