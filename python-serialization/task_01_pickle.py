0444bf98b61b44d1d08f42144e16300cd546f58b
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-05-31 10:16:51 +0200

    fix

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
-"""Trying out pickle module"""
-
+"""1. Pickling Custom Classes"""
 
 
 import pickle
 
+
 class CustomObject:
     def __init__(self, name, age, is_student):
-        """declaring variables"""
         self.name = name
         self.age = age
         self.is_student = is_student
 
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Is Student:", self.is_student)
 
-        def display(self):
-            """display"""
-            print(f"Name: {self.name}")
-            print(f"Age: {self.age}")
-            print(f"Is Student: {self.is_student}")
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except IOError:
+            print("Error: File could not be opened or written")
 
-        def serialize(self, filename):
-            """serializes into JSON
-            handles error if file can't be opened/written
-            """
-            try:
-                with open(filename, "wb") as f:
-                    pickle.dump(self, f)
-            except Exception:
-                print("Error: file cannot be opened or written.")
-                return None
-
-        @classmethod
-        def deserialize(cls, filename):
-            """deserializes
-            raises UnpicklingError if data corrupted
-            or security violation
-            or IOError
-            """
-            try:
-                with open(filename, "rb") as f:
-                    return pickle.load(f)
-            except (IOError, pickle.UnpicklingError):
-                print("Error: File cannot be opened")
-                return None
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except (IOError, pickle.UnpicklingError):
+            print("Error: File could not be opened or contains invalid data")
+            return None
