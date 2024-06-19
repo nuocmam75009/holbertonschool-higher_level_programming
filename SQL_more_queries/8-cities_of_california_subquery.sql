d63330e6c20b3623bdaccbc8255db4a18398ce7e
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 20:27:20 +0200

    retrieve records from a table

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Use database hbtn_0d_usa
+-- list cities of california means ones that have name = 'cali'
+-- but the id can be different.
+-- results in ASC order by cities.id
+USE hbtn_0d_usa;
+SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+ORDER BY id ASC;
