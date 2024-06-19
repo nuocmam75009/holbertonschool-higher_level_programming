cf9afa51bdf4a60832e58cce31bdd44552842d0a
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 14:53:32 +0200

    insert new row

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,6 +1,3 @@
 -- Script that inserts a new row in first_table
 -- Database: hbtn_0c_0
-INSERT INTO first_table (
-    id = 89,
-    name = Best School
-);
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
