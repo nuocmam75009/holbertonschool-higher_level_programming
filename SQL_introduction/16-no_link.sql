b06ef95359f81f85042c396640ce34a841e6799c
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 16:27:59 +0200

    list all records with name value, score then name

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Lists all records of second_table
+-- Don't list rows without 'name' value
+-- Display score then name
+-- Records listed by desc
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY records DESC;
