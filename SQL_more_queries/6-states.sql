6833b8511ed26a2f44a7e88e8472e86b3a5bddf0
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 20:09:13 +0200

    create db and table with id unique & primary

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- create database hbtn_0d_usa
+-- create table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
