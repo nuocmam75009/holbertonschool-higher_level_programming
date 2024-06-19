d57849b3c5d502dcffe9f325baecd08caef6b93b
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 18:56:17 +0200

    list privileges of a user

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script that lists privileges of a user
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0D_2'@'localhost';
+SELECT * FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
+SELECT * FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
