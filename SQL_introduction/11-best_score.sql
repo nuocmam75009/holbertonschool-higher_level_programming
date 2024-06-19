02bdae9608ccce8fad06f3d6177dbd8fd4b77892
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 15:45:00 +0200

    lists all records with score >= 10 in second_table

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records with score >= 10 in second_table
+-- Display score then name
+-- Score in DESC order :)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
