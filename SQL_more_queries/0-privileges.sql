f8cacbc8ff8279222d474f59c349ee5a050e2a2c
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 18:52:44 +0200

    list privileges of a user

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Script that lists privileges of a user
+SHOW GRANTS FOR 'user_0d_1' @'localhost';
+SHOW GRANTS FOR 'user_0D_2' @'localhost';
---
+++ SQL_more_queries/README.md
@@ -0,0 +1,3 @@
+Project SQLLL :))
+More queries now
+Still looking for a damn apprenticeship I am now at 600 applications it's been good
