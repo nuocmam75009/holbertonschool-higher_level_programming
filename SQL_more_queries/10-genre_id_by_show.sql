7be57a3974046a456cfbbacd8e479434fa1c0f0e
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 20:46:30 +0200

    lists tv shows that have one genre linked

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Get data dump from tvshows.sql file
+-- List all shows that have one genre linked
+-- Should display title - genre.id both in ASC order
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
