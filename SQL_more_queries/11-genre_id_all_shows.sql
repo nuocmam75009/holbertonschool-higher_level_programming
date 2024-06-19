5d0910ddc1fddf65f371115843dbede2256c7e44
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 21:14:37 +0200

    lists tv shows + display NULL if no genre

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- Get data dump from tvshows.sql file
+-- List all shows that have one genre linked frm tvshows table
+-- Use INNER JOIN: retrieves data between tables when there's a match
+-- Should display title - genre.id both in ASC order
+-- if a show has no genre display NULL
+USE DATABASE hbtn_0d_tvshows
+SELECT tv_shows.title COALESCE (tv_show_genres.genre_id, NULL) AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, genre_id ASC;
