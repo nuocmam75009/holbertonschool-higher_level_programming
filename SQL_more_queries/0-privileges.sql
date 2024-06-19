986a644f1ba6cc914151862fbe49f4e604b3073c
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 19:06:45 +0200

    list privileges of a user

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- Script that lists privileges of a user
-SHOW GRANTS FOR 'user_0d_1'@'localhost', SHOW GRANTS FOR 'user_0D_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost' SHOW GRANTS FOR 'user_0D_2'@'localhost';
