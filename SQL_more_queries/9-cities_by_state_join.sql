cf76448ddcaad16c31c21a09462ec22f434db1fb
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 20:38:49 +0200

    display cities id, name and states name

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- Display cities.id, cities.name and states.name
+-- cities.id by ASC order
+SELECT cities.id, cities.name, states.name FROM cities;
+JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
