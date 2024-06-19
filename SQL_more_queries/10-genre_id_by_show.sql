866035f67cd0f663f71e560437e1c3bdaec175a9
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 20:53:41 +0200

    lists tv shows that have one genre linked

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,9 @@
 -- Get data dump from tvshows.sql file
 -- List all shows that have one genre linked frm tvshows table
+-- Use INNER JOIN: retrieves data between tables when there's a match
 -- Should display title - genre.id both in ASC order
 USE hbtn_0d_tvshows.sql;
 SELECT tv_shows.title, tv_show_genres.genre_id;
-FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id;
+FROM tv_shows;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id;
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
