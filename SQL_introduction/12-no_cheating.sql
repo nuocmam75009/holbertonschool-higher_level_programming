9f34c4c45c1f2051395038aa9ab7239e694fafda
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 15:55:09 +0200

    update bob score to 10

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,3 @@
 -- Script that updates Bob's score to 10 in second_table
 -- Only show Bob's name field, not ID
-UPDATE second_table;
-SET score = 10 WHERE name = 'Bob';
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
