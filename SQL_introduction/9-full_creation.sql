aa9a5c8eb35e3aab5c67ca3a065481262907c22d
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 15:16:23 +0200

    creates a second table and inserts new rows

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,23 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 )
-INSERT INTO second_table (
-    1,
-    'John',
-    10
-)
-INSERT INTO second_table (
-    2,
-    'Alex',
-    3
-)
-INSERT INTO second_table (
-    3,
-    'Bob',
-    14
-)
-INSERT INTO second_table (
-    4,
-    'George',
-    8
-);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
