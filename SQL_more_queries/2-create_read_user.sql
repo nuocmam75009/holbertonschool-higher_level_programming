25f541302712ebc3ac40300543fafb959324aca9
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-19 19:29:00 +0200

    reate DB and user with SELECT

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Script that creates database hbtn_0d_2 and user user_0d_2
+-- user_0d_2 only has READ privileges
+-- user's pwd set to user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
