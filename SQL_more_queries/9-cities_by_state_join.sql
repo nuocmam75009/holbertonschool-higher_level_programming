717c9f34d35d46f418e1fb02d7c0eed070b42e92
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 09:03:07 +0200

    create db and add user + grant privileges

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,10 @@
 -- Display cities.id, cities.name and states.name
 -- cities.id by ASC order
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT
+    cities.id, cities.name, states.name;
+FROM
+    cities;
+JOIN
+    states ON cities.state_id = states.id;
+ORDER BY
+    cities.id ASC;
