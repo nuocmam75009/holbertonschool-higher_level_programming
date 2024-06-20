dd9cf3a64734cd8f34bb96557bb694ee0d60d6b3
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 08:33:14 +0200

    first task list privileges

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,7 @@
 -- Use INNER JOIN: retrieves data between tables when there's a match
 -- Should display title - genre.id both in ASC order
 -- if a show has no genre display NULL
+-- Coalesce function returns first non-null value
 USE DATABASE hbtn_0d_tvshows
 SELECT tv_shows.title COALESCE (tv_show_genres.genre_id, NULL) AS genre_id
 FROM tv_shows
