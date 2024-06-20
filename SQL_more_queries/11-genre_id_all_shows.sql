634a71c4fa64dac36f52c8fb13227218d7db46a2
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 09:58:41 +0200

    tv shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,8 +4,7 @@
 -- Should display title - genre.id both in ASC order
 -- if a show has no genre display NULL
 -- Coalesce function returns first non-null value
-USE DATABASE hbtn_0d_tvshows
-SELECT tv_shows.title COALESCE (tv_show_genres.genre_id, NULL) AS genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, genre_id ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
