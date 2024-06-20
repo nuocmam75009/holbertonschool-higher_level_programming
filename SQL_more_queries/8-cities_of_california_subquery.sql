e33200f3ab1bad2d8d484a954d8ba3c53cb3bb7e
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 09:11:35 +0200

    lists cities of california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,10 @@
 -- list cities of california means ones that have name = 'cali'
 -- but the id can be different.
 -- results in ASC order by cities.id
-SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT
+    id, name
+FROM
+    cities
+WHERE
+    state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
