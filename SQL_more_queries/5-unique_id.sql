9347b24eaa7dcab835f24622aecff6cbe467ce17
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 09:09:12 +0200

    create table with id INT unique

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Create table unique_id hihihihihihihihihohohohohohohohohohoh)ergpeqf^pqfks
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT  DEFAULT 1 UNIQUE,
-    name VARCHAR(256),
+    name VARCHAR(256)
 );
