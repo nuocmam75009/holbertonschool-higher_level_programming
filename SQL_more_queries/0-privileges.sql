8bac2f770af02ee64361ad382b4f4611fb39cc34
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 08:37:03 +0200

    first task list privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- Script that lists privileges of a user
+SELECT user, host FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
+SELECT user, host FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0D_2'@'localhost';
