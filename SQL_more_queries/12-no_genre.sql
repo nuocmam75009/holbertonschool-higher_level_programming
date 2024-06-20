ad9c4b70f4dec9521cbb60c10d4e87269989d16c
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 10:23:52 +0200

    tv shows

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title COALESCE(tv_show_genres.genre_id, -1) AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show.genre.tv_show_id
-WHERE tv_show_genres.genre_id IS NULL
+-- WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, genre_id ASC;
