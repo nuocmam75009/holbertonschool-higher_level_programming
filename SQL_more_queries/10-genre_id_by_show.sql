c130fd597fe9c4cb284b69193f2df8fd549a4393
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 11:10:38 +0200

    tv shows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@
 -- Should display title - genre.id both in ASC order
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows.tv_shows
-INNER JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
