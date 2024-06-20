00960097f31c7a58c791eebdabdae9c1be0556d0
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 08:51:07 +0200

    create db and add user + grant privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,5 +2,6 @@
 -- user_0d_2 only has READ privileges
 -- user's pwd set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2* TO 'user_0d_2'@'localhost';
