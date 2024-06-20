a9b63f989090b8436e887913be2709872ac26da2
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 20:25:07 +0200

    list states and connect to loocalhost port 3306

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,15 +7,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          username=sys.argv[1], password=sys.argv[2],
                          db=sys.argv[3])
-
     cur = db.cursor()
-    cur.execute("SELECT name FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     states = cur.fetchall()
-
     for state in states:
         print(state)
-
-    if cur:
-        cur.close()
-    if db:
-        db.close()
+    cur.close()
+    db.close()
