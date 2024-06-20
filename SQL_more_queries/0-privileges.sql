6805c432b56e2ebd13f34bcab84c20eb106ab89e
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 13:37:17 +0200

    fix

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- 3 first students in the Batch ID=3 user_0d_1 user_0D_2
 -- because Batch 3 is the best!d
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0D_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,8 @@
 -- From database hbtn_0d_tvshows
 -- List all shows without a genre
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows.tv_shows
+FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.id IS NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
