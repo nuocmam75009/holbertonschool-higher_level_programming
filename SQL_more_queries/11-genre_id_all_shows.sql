98f8c59fc663c78f675cea28bf70afe74f71669b
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 13:29:56 +0200

    fix lucas

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,10 +3,10 @@
 -- Use INNER JOIN: retrieves data between tables when there's a match
 -- Should display title - genre.id both in ASC order
 -- if a show has no genre display NULL
--- Coalesce function returns first non-null value
+
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows.tv_shows
-LEFT JOIN tv_show_genres.hbtn_0d_tvshows
+FROM tv_shows
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
