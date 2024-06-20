751ad3b98863db3e40c8da23508b25ab532a2cda
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 10:11:59 +0200

    tv shows

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,4 +2,6 @@
 -- List all shows without a genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-WHERE tv_show_genres.genre_id IS NULL;
+JOIN tv_show_genres ON tv_shows.id = tv_show.genre.tv_show_id
+-- WHERE tv_show_genres.genre_id IS NULL;
+ORDER BY tv_shows.title, genre_id;
