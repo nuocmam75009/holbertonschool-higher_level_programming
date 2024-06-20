7b780fa9581831f34bcc2f8f69d6aa854f38dc08
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 12:08:20 +0200

    show grants

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Script that lists privileges of a user
--- SELECT user, host FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0D_2'@'localhost';
