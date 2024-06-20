a793b5c8cd627f79735690eb03f9e10f419fb193
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 09:43:47 +0200

    lists cities of california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -7,5 +7,5 @@ SELECT
 FROM
     `cities`
 WHERE
-    `state_id` IN = (SELECT `id` FROM `states` WHERE name = 'California')
+    `state_id` IN = (SELECT `id` FROM `states` WHERE `name` = 'California')
 ORDER BY `id` ASC;
