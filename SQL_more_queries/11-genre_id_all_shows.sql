315ad938f2970a28df63c74857f472542289a941
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 11:02:20 +0200

    tv shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,6 +5,8 @@
 -- if a show has no genre display NULL
 -- Coalesce function returns first non-null value
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
--- ORDER BY tv_shows.title, tv_show_genres.genre_id;
+FROM hbtn_0d_tvshows.tv_shows
+LEFT JOIN tv_show_genres.hbtn_0d_tvshows
+ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
