adf1ec7b26f9362bfe1811c2331b81a2b987fbdb
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 08:39:41 +0200

    first task list privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- Script that lists privileges of a user
-SELECT user, host FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+-- SELECT user, host FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
+-- CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+-- CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0D_2'@'localhost';
