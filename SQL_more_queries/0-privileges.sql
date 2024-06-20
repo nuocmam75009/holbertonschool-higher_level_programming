7a6e7505ab756359d1365ef04ea842a08e66af9d
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 12:10:19 +0200

    show grants

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
+-- because Batch 3 is the best!d
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0D_2'@'localhost';
