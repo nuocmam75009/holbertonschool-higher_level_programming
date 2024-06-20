f5c37393084547757717da96e764066a89804339
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-20 13:57:34 +0200

    task13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- From database hbtn_0d_tvshows
+-- List all genres from hbtn_0d_tvshows and display nuumber of shows linked to each
+-- TV SHOW GENRE -> Num of shows
+SELECT tv_genres.name AS 'genre' COUNT(*) AS 'number_of_shows'
+FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = genre_id
+GROUP BY genre_id
+ORDER BY COUNT(*) DESC;
