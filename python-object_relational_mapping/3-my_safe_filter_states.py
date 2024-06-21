3d6325facb70cde4c3a2fd9f7ac9e9224d04d5ab
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-21 09:36:52 +0200

    same as 2 but safe SQL

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,6 +3,7 @@ import MySQLdb
 import sys
 # Lists states where name matches arg
 # Sys.Args: username, password, db, state
+# Safe from SQL injection -> not in SQL query
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -13,11 +14,12 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM STATES WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states")
     query_rows = cur.fetchall()
 
     for row in query_rows:
-        print(row)
+        if row[1] == sys.argv[4]:
+            print(row)
 
     cur.close()
     db.close()
