08e1dcb1004d07fb9e31231f6fe6f33706174956
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-21 17:49:11 +0200

    14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 -- From database hbtn_0d_tvshows
 -- List all genres from hbtn_0d_tvshows and display nuumber of shows linked to each
 -- TV SHOW GENRE -> Num of shows
-SELECT name
+SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
-ON name.id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
