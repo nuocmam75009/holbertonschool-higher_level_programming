38ea3aa21c2fadb2351b4f0a99155ee252245bc6
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-21 10:51:56 +0200

    try vim

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,12 +5,12 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
@@ -21,3 +21,4 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
+
