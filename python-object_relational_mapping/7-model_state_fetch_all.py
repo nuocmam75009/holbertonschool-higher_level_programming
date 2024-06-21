129d20df0b09ccb63405aa7718f9c5b184d08263
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-21 16:59:16 +0200

    list all states via alchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,13 +12,13 @@ def print_states():
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
-
     session = Session()
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
     engine.dispose()
 
-
 if __name__ == "__main__":
+
+
     print_states()
