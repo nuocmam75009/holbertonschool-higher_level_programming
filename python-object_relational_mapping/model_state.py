b0f5280172338534ee98cc76993c08ba767160b6
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-06-22 18:23:06 +0200

    document

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,7 +4,7 @@
         linked to 'states' table of MySQL server
         MySQL server running on localhost port 3306
 """
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -12,19 +12,16 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class State(Base):
+    """State class inheriting from Base."""
     __tablename__ = 'states'
 
     id = Column(
         Integer,
-            primary_key=True,
-            autoincrement=True,
-            nullable=False
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
         )
     name = Column(
         String(128),
         nullable=False
         )
-
-engine = create_engine('mysql://root:lucaslucas@localhost:3306/hbtn_0e_6_usa', echo=False)
-Session = sessionmaker(bind=engine)
-session = Session()
